a428e2d845122ee640c74ab70f4fa6bd7fe7d5b8
Nick Inman <nick@theoatmeal.com>
2019-02-14 21:02:29 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -25,13 +25,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-	    params.require(:recipe).permit(
-	      :title,
-	      ingredients_attributes: [
-	        :name,
-	        :quantity
-	      ]
-	    )
+	    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
 	  end
 
 end
