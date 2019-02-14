2793ed8be547ecfc7a05d045dcaaaf4ce9ea9d96
Nick Inman <nick@theoatmeal.com>
2019-02-14 20:47:28 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,24 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "1")
+    @recipe.ingredients.build(name: "2")
+    #binding.pry
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.valid?
+      @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredient_attributes: [:name, :quantity])
   end
 end
